36e0ff56247fcf6c4e0ef892b8da83e0eb7dfdb5
demo07 <dddemesvar07@gmail.com>
2019-05-04 18:00:07 +0000

    commit

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -124,11 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = '/users/home/'
 LOGOUT_REDIRECT_URL = '/users/home/'
 AUTH_USER_MODEL = 'users.CustomUser'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
